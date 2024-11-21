5b328030363cb8d1f92e41cbcdd7baf12f6c6411
Apple <apple@Apples-MacBook-Pro.local>
2024-11-21 23:46:31 +0200

    Updates github workflow

--- todo_project/todo_project/settings.py
+++ todo_project/todo_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-_+qs^6szdzf@^#fh8=hw*+@1_%q(3sr4xghzlqt1j&@g^&r*$_"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
